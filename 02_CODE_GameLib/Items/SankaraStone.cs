b1b855ae8a54b575cfd4b0c6320fb4eb23897ce2
Job <LordChunk@users.noreply.github.com>
2020-12-07 15:44:45 +0100

    Added SankaraStone

---
+++ 02_CODE_GameLib/Items/SankaraStone.cs
@@ -0,0 +1,13 @@
+﻿using CODE_GameLib.Interfaces;
+using CODE_GameLib.Items.Common;
+
+namespace CODE_GameLib.Items
+{
+    public class SankaraStone : Item, IPickUpItem
+    {
+        public void OnPickUp()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
